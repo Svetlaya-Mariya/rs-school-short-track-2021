e79884b9331d9a0e92004f47358e33734f5ef96f
Мария Светлая <merry76light@gmail.com>
2021-04-09 00:13:57 +0300

    common charackter count and sort

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,29 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const newArr = [];
+  const arr1 = arr;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      newArr.push(arr[i]);
+    }
+  }
+  const calcNum = function(a, b) {
+    return a - b;
+  };
+  
+  newArr.sort(calcNum);
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== -1) {
+      for (let j = 0; j < newArr.length; j++) {
+        arr1[i] = newArr[j];
+        newArr.shift();
+        break;
+      }
+    }
+  }
+  return arr1;
 }
 
 module.exports = sortByHeight;
