d710ac91eba03645a4e22b68a52090fecd0144a4
Мария Светлая <merry76light@gmail.com>
2021-04-10 00:30:55 +0300

    mac-addres

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,23 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const addres = n;
+  const arr = addres.split('-');
+  const regExp = /^[0-9A-F]{2}$/;
+  let result;
+  let res = 1;
+  for (let i = 0; i < arr.length; i++) {
+    res = 0;
+    if (regExp.test(arr[i]) === false) {
+      result = false;
+      break;
+    } else res = 1;
+  }
+  if (res === 1) {
+    result = true;
+  }
+  return result;
 }
 
 module.exports = isMAC48Address;
