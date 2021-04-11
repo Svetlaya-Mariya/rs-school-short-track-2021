e53b6fe5661c9f2795966139867b116d583bc6d2
Мария Светлая <merry76light@gmail.com>
2021-04-11 21:12:59 +0300

    file names & sum digit

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,20 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const fileNames = names;
+  const arr = [];
+  const obj = {};
+  for (let i = 0; i < fileNames.length; i++) {
+    if (arr.includes(fileNames[i])) {
+      obj[fileNames[i]] = obj[fileNames[i]] || 1;
+      const newName = `${fileNames[i]}(${obj[fileNames[i]]++})`;
+      arr.push(newName);
+    } else {
+      arr.push(fileNames[i]);
+    }
+  }
+  return arr;
 }
 
 module.exports = renameFiles;
--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,17 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  const num = String(n);
+  let arr = num.split('');
+  let res;
+  while (arr.length !== 1) {
+    const arr1 = arr.map(Number);
+    const reducer = (a, b) => a + b;
+    res = arr1.reduce(reducer);
+    arr = String(res).split('');
+  }
+  return res;
 }
 
 module.exports = getSumOfDigits;
